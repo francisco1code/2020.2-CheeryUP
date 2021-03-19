8c27158291a3042b103b5e08c37cea13e4a1cdde
fernandes-natanael <filhonatanael01@gmail.com>
2021-03-19 03:25:29 -0300

    docs_feat_modificacao do usuario django #57

--- src/Project_CheeryUp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Paciente
-# Register your models here.
-
-class PacienteAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Paciente, PacienteAdmin)
