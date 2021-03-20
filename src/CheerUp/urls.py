63e59048fc5a64f5d86e4f19b499171de8fe4aca
Durval Carvalho <dudurval2@gmail.com>
2021-03-20 00:29:15 -0300

    Fix endpoints

--- src/CheerUp/urls.py
+++ src/CheerUp/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from paciente.views import PacienteViewSet
+# from paciente.views import PacienteViewSet
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'pacientes', PacienteViewSet)
+from paciente.urls import router as paciente_router
+from users.urls import router as psicologo_router
 
+router = routers.DefaultRouter()
+router.registry.extend(paciente_router.registry)
+router.registry.extend(psicologo_router.registry)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/register/', include('users.urls')),
+    path('api/', include(router.urls)),
+    # path('api/register/', include('users.urls')),
 ]
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,8 +1,8 @@
-  
+
 from rest_framework import serializers
 from .models import Psicologo
 
-class UsersSerializer(serializers.Serializer):
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Psicologo
         fields = ('user', 'name', 'email', 'nCRP', 'bio', 'genero')
